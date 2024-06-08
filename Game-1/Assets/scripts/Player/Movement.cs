51caa41bdbc160b12ac4388281ddb6125489e10b
GAMERDUHS <101351258+GAMERDUHS@users.noreply.github.com>
2024-06-08 22:00:16 +0200

    V10

--- Game-1/Assets/scripts/Player/Movement.cs
+++ Game-1/Assets/scripts/Player/Movement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    private bool facingRight = true;
 
     void Start()
     {
@@ -17,11 +18,32 @@ public class PlayerMovement : MonoBehaviour
         // Eingaben lesen
         movement.x = Input.GetAxisRaw("Horizontal"); // A und D oder Pfeiltasten links/rechts
         movement.y = Input.GetAxisRaw("Vertical");   // W und S oder Pfeiltasten hoch/runter
+
+        // Check direction and flip sprite if necessary
+        if (movement.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (movement.x < 0 && facingRight)
+        {
+            Flip();
+        }
     }
 
     void FixedUpdate()
     {
         // Physik-basierte Bewegung
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        // Toggle the value of facingRight
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1 to flip the sprite
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
